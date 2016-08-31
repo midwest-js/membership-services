bcd2666a819b7602135b5d1129db3bc01b027d4e
Linus Miller <lohfu@lohfu.io>
2016-08-31 11:15:50 +0200

    Unwrap [] from middleware

--- services/users/routes.js
+++ services/users/routes.js
@@ -5,13 +5,13 @@ const mw = require('./middleware')
 const isAuthenticated = require('../../passport/authorization-middleware').isAuthenticated
 
 module.exports = [
-  [ '/api/users/', 'get', [ isAuthenticated, mw.formatQuery, mw.query ] ],
-  [ '/api/users/', 'post', [ mw.register ]],
-  [ '/api/users/me', 'get', [ mw.getCurrent ] ],
-  [ '/api/users/:id', 'delete', [ isAuthenticated, mw.remove ]],
-  [ '/api/users/:id', 'get', [ isAuthenticated, mw.findOne ] ],
-  [ '/api/users/:id', 'patch', [ isAuthenticated, mw.update ] ],
-  [ '/api/users/:id', 'put', [ isAuthenticated, mw.update ] ],
-  [ '/api/users/reset-password', 'post', [ mw.resetPassword ] ],
-  [ '/api/users/update-password', 'post', [ mw.updatePassword ] ],
+  [ '/api/users/', 'get', isAuthenticated, mw.formatQuery, mw.query ],
+  [ '/api/users/', 'post', mw.register ],
+  [ '/api/users/me', 'get', mw.getCurrent ],
+  [ '/api/users/:id', 'delete', isAuthenticated, mw.remove],
+  [ '/api/users/:id', 'get', isAuthenticated, mw.findOne ],
+  [ '/api/users/:id', 'patch', isAuthenticated, mw.update ],
+  [ '/api/users/:id', 'put', isAuthenticated, mw.update ],
+  [ '/api/users/reset-password', 'post', mw.resetPassword ],
+  [ '/api/users/update-password', 'post', mw.updatePassword ]
 ]
