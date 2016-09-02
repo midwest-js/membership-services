bc07195fadc3d654a02917947e0dac2feb43e0bc
Linus Miller <lohfu@lohfu.io>
2016-09-02 12:06:32 +0200

    Save RegExp object to database instead of string, and more concise filter

--- services/permissions/model.js
+++ services/permissions/model.js
@@ -3,25 +3,22 @@
 const mongoose = require('mongoose')
 
 const PermissionSchema = new mongoose.Schema({
-  regex: { type: String, required: true, unique: true },
+  regex: { type: Object, required: true, unique: true },
   roles: { type: [ String ], required: true },
   dateCreated: { type: Date, default: Date.now }
 })
 
 PermissionSchema.statics.findMatches = function (email, cb) {
-  const collected = []
-
-  this.find({}, function (err, permissions) {
+  this.find({}, (err, permissions) => {
     if (err) cb(err)
 
-    for (let i = 0; i < permissions.length; i++) {
-      const regex = new RegExp(permissions[i].regex)
-
-      if (email.search(regex) > -1) collected.push(permissions[i])
-    }
-
-    cb(null, collected)
+    // return only permissions whose regex match the email
+    cb(null, permissions ? permissions.filter(permission => permission.regex.test(email)) : null)
   })
 }
+
+PermissionSchema.path('regex').set(function (regex) {
+  return regex instanceof RegExp ? regex : new RegExp(regex)
+})
 
 module.exports = mongoose.model('Permission', PermissionSchema)
