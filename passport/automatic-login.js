f0f912e2507832d655d95037761f78f2cf56ade6
Linus Miller <lohfu@lohfu.io>
2016-09-05 18:09:26 +0200

    Require user model file directly and do not login if user not found

--- passport/automatic-login.js
+++ passport/automatic-login.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const mongoose = require('mongoose')
-
 /* Middleware simply for simplying when in development
  * mode so that you do not have to re-login everytime the
  * node server reboots. set global.LOGIN_USER to the user you wish
  * to automatically login as.
  */
 
+const User = require('../services/users/model')
+
 module.exports = function automaticLogin(req, res, next) {
   if (ENV === 'development' && global.LOGIN_USER && !req.user) {
-    const User = mongoose.model('User')
-
     User.findOne({ email: LOGIN_USER }, (err, user) => {
       if (err) return next(err)
+
+      if (!user) return next()
 
       req.login(user, (err) => {
         if (err) return next(err)
