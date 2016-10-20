88e7bb02843e73e6e8a36af24459b01761eb90f4
Linus Miller <lohfu@lohfu.io>
2016-10-20 13:46:54 +0200

    Added verifyEmail path in config

--- sample-config/membership.js
+++ sample-config/membership.js
@@ -22,6 +22,7 @@ module.exports = {
     login: '/login',
     forgotPassword: '/forgot-password',
     updatePassword: '/change-password',
+    verifyEmail: '/verify-email',
   },
 
   redirects: {
