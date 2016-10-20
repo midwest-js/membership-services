6b24a63f5d21b28778cc798c1368ce78e2299dd0
Linus Miller <lohfu@lohfu.io>
2016-10-20 13:21:44 +0200

    Convert RegExp to strings by using RegExp.prototype.source

--- services/permissions/middleware.js
+++ services/permissions/middleware.js
@@ -6,7 +6,31 @@ const paginate = require('midwest/middleware/paginate');
 
 const Permission = require('./model');
 
-module.exports = Object.assign(rest(Permission), {
+const mw = rest(Permission);
+
+module.exports = Object.assign({}, mw, {
   formatQuery: formatQuery(['limit', 'sort', 'page']),
   paginate: paginate(Permission, 20),
+  findById: (req, res, next) => {
+    mw.findById(req, res, (err) => {
+      if (err) return next(err);
+
+      if (res.locals.permission) {
+        res.locals.permission.regex = res.locals.permission.regex.source;
+      }
+
+      next();
+    });
+  },
+  query: (req, res, next) => {
+    mw.query(req, res, (err) => {
+      if (err) return next(err);
+
+      res.locals.permissions.forEach((permission) => {
+        permission.regex = permission.regex.source;
+      });
+
+      next();
+    });
+  },
 });
