d0ea31da34fdeeabfbf2ba0464c0089d62927e48
Linus Miller <lohfu@lohfu.io>
2016-12-20 10:17:26 +0100

    Comment out custom de-/serializeUser code for now

--- passport/setup.js
+++ passport/setup.js
@@ -1,32 +1,33 @@
 'use strict';
 
-const p = require('path');
+// const p = require('path');
 
 const _ = require('lodash');
 const passport = require('passport');
 
 // const User = require('../services/users/model');
 
-const config = require(p.join(process.cwd(), 'server/config/membership'));
+// const config = require(p.join(process.cwd(), 'server/config/membership'));
 
 let serializeUser;
 let deserializeUser;
 
-if (typeof config.serializeUser === 'function') {
-  if (config.serializeUser.length === 1) {
-    serializeUser = config.serializeUser(User);
-  } else {
-    serializeUser = config.serializeUser;
-  }
-}
+// if (typeof config.serializeUser === 'function') {
+//   if (config.serializeUser.length === 1) {
+//     serializeUser = config.serializeUser(User);
+//   } else {
+//     serializeUser = config.serializeUser;
+//   }
+// }
 
-if (typeof config.deserializeUser === 'function') {
-  if (config.deserializeUser.length === 1) {
-    deserializeUser = config.deserializeUser(User);
-  } else {
-    deserializeUser = config.deserializeUser;
-  }
-}
+// if (typeof config.deserializeUser === 'function') {
+//   if (config.deserializeUser.length === 1) {
+//     deserializeUser = config.deserializeUser(User);
+//   } else {
+//     deserializeUser = config.deserializeUser;
+//   }
+// }
+
 // used to serialize the user for the session
 passport.serializeUser(serializeUser || ((user, done) => {
   done(null, _.pick(user, ['id', 'username', 'email', 'roles']));
