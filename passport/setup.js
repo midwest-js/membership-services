3cbde791bf163f842e7802b1a99f965727cd21aa
Linus Miller <lohfu@lohfu.io>
2017-01-05 16:19:30 +0100

    Enable passing custom columns to be serialized

--- passport/setup.js
+++ passport/setup.js
@@ -1,17 +1,26 @@
 'use strict';
 
-// const p = require('path');
+const p = require('path');
 
 const _ = require('lodash');
 const passport = require('passport');
 
 // const User = require('../services/users/model');
 
-// const config = require(p.join(process.cwd(), 'server/config/membership'));
+const config = require(p.join(process.cwd(), 'server/config/membership'));
 
 let serializeUser;
 let deserializeUser;
 
+const pick = ['id', 'username', 'email', 'roles'];
+
+if (typeof config.serializeUser === 'function') {
+  serializeUser = config.serializeUser;
+}
+
+if (Array.isArray(config.serializeColumns)) {
+  pick.push(...config.serializeColumns);
+}
 // if (typeof config.serializeUser === 'function') {
 //   if (config.serializeUser.length === 1) {
 //     serializeUser = config.serializeUser(User);
@@ -30,15 +39,12 @@ let deserializeUser;
 
 // used to serialize the user for the session
 passport.serializeUser(serializeUser || ((user, done) => {
-  done(null, _.pick(user, ['id', 'username', 'email', 'roles']));
+  done(null, _.pick(user, pick));
 }));
 
 // used to deserialize the user
 passport.deserializeUser(deserializeUser || ((user, done) => {
   done(null, user);
-  // User.findById(user, (err, user) => {
-  //   done(err, user);
-  // });
 }));
 
 require('./req');
