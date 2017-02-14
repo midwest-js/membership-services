3f1cf9b15088af54071112456e5f712ad9fe112a
Linus Miller <lohfu@lohfu.io>
2017-02-14 17:17:04 +0100

    Use new midwest transaction functionality

--- services/users/handlers.js
+++ services/users/handlers.js
@@ -5,7 +5,7 @@ const p = require('path');
 const _ = require('lodash');
 
 const factory = require('midwest/factories/handlers');
-const { columns: sqlColumns, where } = require('midwest/util/sql');
+const { columns: sqlColumns } = require('midwest/util/sql');
 
 const db = require(p.join(process.cwd(), 'server/db'));
 const config = require(p.join(process.cwd(), 'server/config/membership'));
@@ -22,7 +22,7 @@ if (config.userColumns) {
 const handlers = {
   users: factory({
     table: 'users',
-    columns: columns,
+    columns,
     exclude: ['create', 'replace', 'update'],
   }),
   emailTokens: require('../email-tokens/handlers'),
@@ -100,59 +100,40 @@ SELECT role_id, user_id FROM inserted_rows;
 const columnsString = sqlColumns(columns);
 
 function update(id, json, cb) {
-  db.connect((err, client, done) => {
-    client.query('BEGIN;', (err) => {
-      if (err) {
-        db.rollback(client, done);
-        return cb(err);
+  db.begin((err, transaction) => {
+    if (err) return cb(err);
+
+    let roles = json.roles;
+
+    json = _.pickBy(json, (value, key) => key !== 'roles' && columns.includes(key));
+
+    const keys = _.keys(json).map((key) => `"${_.snakeCase(key)}"`);
+    const values = _.values(json);
+
+    const query = `UPDATE users SET ${keys.map((key, i) => `${key}=$${i + 1}`).join(', ')} WHERE id = $${keys.length + 1} RETURNING ${columnsString};`;
+
+    transaction.query(query, [...values, id], (err, result) => {
+      if (err) return cb(err);
+
+      if (roles && roles.length) {
+        if (typeof roles[0] === 'object') roles = roles.map((role) => role.id);
+
+        transaction.query(updateRolesQuery, [id, roles], (err) => {
+          if (err) return cb(err);
+
+          transaction.commit((err) => {
+            if (err) return cb(err);
+
+            handlers.users.findById(id, cb);
+          });
+        });
+      } else {
+        transaction.commit((err) => {
+          if (err) return err;
+
+          cb(null, result.rows[0]);
+        });
       }
-
-      let roles = json.roles;
-
-      json = _.pickBy(json, (value, key) => key !== 'roles' && columns.includes(key));
-
-      const keys = _.keys(json).map((key) => `"${_.snakeCase(key)}"`);
-      const values = _.values(json);
-
-      const query = `UPDATE users SET ${keys.map((key, i) => `${key}=$${i + 1}`).join(', ')} WHERE id = $${keys.length + 1} RETURNING ${columnsString};`;
-
-      console.log(query);
-
-      client.query(query, [...values, id], (err, result) => {
-        if (err) {
-          db.rollback(client, done);
-          return cb(err);
-        }
-
-        if (roles && roles.length) {
-          if (typeof roles[0] === 'object') roles = roles.map((role) => role.id);
-
-          client.query(updateRolesQuery, [id, roles], (err, result) => {
-            if (err) {
-              db.rollback(client, done);
-              return cb(err);
-            }
-
-            client.query('COMMIT;', (err) => {
-              if (err) {
-                db.rollback(client, done);
-                return cb(err);
-              }
-
-              handlers.users.findById(id, cb);
-            });
-          });
-        } else {
-          client.query('COMMIT;', (err) => {
-            if (err) {
-              db.rollback(client, done);
-              return cb(err);
-            }
-
-            cb(null, result.rows[0]);
-          });
-        }
-      });
     });
   });
 }
