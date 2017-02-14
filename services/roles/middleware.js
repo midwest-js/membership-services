8e4b961dec73f8b8ac89512cb34b34aa92a9c6a4
Linus Miller <lohfu@lohfu.io>
2017-02-14 17:15:11 +0100

    Set createdById

--- services/roles/middleware.js
+++ services/roles/middleware.js
@@ -6,10 +6,19 @@ const paginate = require('midwest/factories/paginate');
 
 const handlers = require('./handlers');
 
-module.exports = Object.assign(factory({
+const mw = factory({
   plural: 'roles',
-  handlers: handlers,
-}), {
+  handlers,
+});
+
+module.exports = Object.assign({}, mw, {
+  create(req, res, next) {
+    Object.assign(req.body, {
+      createdById: req.user.id,
+    });
+
+    mw.create(req, res, next);
+  },
   formatQuery: formatQuery(['limit', 'sort', 'page']),
   paginate: paginate(handlers.count, 20),
 });
